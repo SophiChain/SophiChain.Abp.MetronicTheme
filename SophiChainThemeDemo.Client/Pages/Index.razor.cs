4fb67800cf51817f6815c167c81dd3b5a237f8ba
dev-mansour <mansour.sarhani@gmail.com>
2025-06-07 11:20:50 +0330

    Updated Message, Notification, Alert

--- SophiChainThemeDemo.Client/Pages/Index.razor.cs
+++ SophiChainThemeDemo.Client/Pages/Index.razor.cs
@@ -11,8 +11,24 @@ public partial class Index
     public PageToolbar Toolbar { get; set; } = new PageToolbar();
     public List<BreadcrumbItem> CustomBreadcrumb { get; set; }
 
+    public bool IsFilterOpen { get; set; } = true;
+
+    public class TreeItem
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+
+    public IEnumerable<TreeItem> Data { get; set; }
+
     protected override async Task OnInitializedAsync()
     {
+        List<TreeItem> items = new List<TreeItem>();
+        items.Add(new TreeItem() { Id = 0, Text = "فیلترها" });
+        Data = items;
+
+
+
         GetToolbar();
 
         CustomBreadcrumb = new List<BreadcrumbItem>()
@@ -27,6 +43,42 @@ public partial class Index
     private async Task ShowSnack()
     {
         await Notify.Warn("این یک متن هشدار برای نوتیفیکیشن است.");
+    }
+
+    private async Task ShowMessage()
+    {
+        //var confirmed = await Message.Confirm(
+        //    "این یک متن برای سرویس مسج است.", 
+        //    "هشدار",
+        //    (options) =>
+        //    {
+        //        options.MessageIcon = "fa-solid fa-home";
+        //        options.OkButtonText = "اوکی";
+        //        options.ConfirmButtonText = "تایید";
+        //        options.CancelButtonText = "انصراف";
+
+        //    });
+        //if (confirmed)
+        //{
+        //    await Notify.Warn("این یک متن هشدار برای نوتیفیکیشن است.");
+        //}
+
+        await Message.Warn(
+            "این یک متن هشدار برای مسج است.",
+            "هشدار",
+            (options) =>
+            {
+                options.OkButtonText = "فهمیدم";
+            }
+        );
+    }
+
+    private void ShowAlert()
+    {
+        Alerts.Warning("این یک متن هشدار برای آلرت است.", "هشدار");
+        Alerts.Success("این یک متن هشدار برای آلرت است.", "هشدار");
+        Alerts.Info("این یک متن هشدار برای آلرت است.", "هشدار");
+        Alerts.Danger("این یک متن هشدار برای آلرت است.", "هشدار");
     }
 
     private void GetToolbar()
