84767dee904cb21737c095f9e14e16770e19faa1
dev-mansour <mansour.sarhani@gmail.com>
2025-06-07 15:31:45 +0330

    [fix] Auth Alert

---
+++ SophiChainThemeDemo.Client/Pages/SignUp.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SophiChainThemeDemo.Pages;
+public partial class SignUp
+{
+    [Inject] NavigationManager navigationManager { get; set; }
+
+    public string FormState { get; set; } = default!;
+    public bool OTPSent { get; set; }
+
+    
+    private string Captcha = "";
+    public string CaptchaInput = "";
+
+    protected override Task OnInitializedAsync()
+    {
+        FormState = "password-login";
+
+        return base.OnInitializedAsync();
+    }
+    
+
+    private async Task LoginWithOtp()
+    {
+        FormState = "otp-login";
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private async Task LoginWithPassword()
+    {
+        FormState = "password-login";
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private async Task OnForgotPassword()
+    {
+        FormState = "reset-password";
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private async Task OnSendPasswordResetLink()
+    {
+        Alerts.Success("لینک بازیابی رمز عبور برای شما ارسال شد.");
+    }
+}
