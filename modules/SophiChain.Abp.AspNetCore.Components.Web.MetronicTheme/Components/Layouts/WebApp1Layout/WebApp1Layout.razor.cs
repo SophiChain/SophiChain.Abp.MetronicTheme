86bcd50d54c087dfc99493b05cfd9cf874cce137
dev-mansour <mansour.sarhani@gmail.com>
2025-06-08 14:56:06 +0330

    [fix] App Loader

--- modules/SophiChain.Abp.AspNetCore.Components.Web.MetronicTheme/Components/Layouts/WebApp1Layout/WebApp1Layout.razor.cs
+++ modules/SophiChain.Abp.AspNetCore.Components.Web.MetronicTheme/Components/Layouts/WebApp1Layout/WebApp1Layout.razor.cs
@@ -9,24 +9,20 @@ public partial class WebApp1Layout
     [CascadingParameter(Name = "ThemeState")]
     public ThemeCascadingState ThemeState { get; set; } = new();
 
-    public bool IsLoading { get; set; } = true;
-    public string? IsLoadingText { get; set; }
-
-
     private IKTThemeHelpers KTHelper = default!;
     public bool SidebarMinimizeState;
+    public bool IsLoading { get; set; } = true;
 
     protected override void OnInitialized()
     {
         KTHelper = new KTThemeHelpers(JS);
+        KTHelper.addBodyAttribute("data-kt-app-page-loading", "on");
     }
 
     protected override void OnAfterRender(bool firstRender)
     {
         if (firstRender)
         {
-            KTHelper.addBodyClass("app-default");
-
             KTHelper.addBodyAttribute("data-kt-app-header-fixed", "true");
             KTHelper.addBodyAttribute("data-kt-app-sidebar-fixed", "true");
             KTHelper.addBodyAttribute("data-kt-app-sidebar-hoverable", "true");
@@ -34,6 +30,8 @@ public partial class WebApp1Layout
             KTHelper.addBodyAttribute("data-kt-app-sidebar-push-toolbar", "true");
             KTHelper.addBodyAttribute("data-kt-app-sidebar-push-footer", "true");
             KTHelper.addBodyAttribute("data-kt-app-toolbar-enabled", "true");
+
+            KTHelper.addBodyClass("app-default");
 
             JS.InvokeVoidAsync("KTModalUpgradePlan.init");
             JS.InvokeVoidAsync("KTCreateApp.init");
@@ -45,41 +43,41 @@ public partial class WebApp1Layout
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
-        if (firstRender)
+        var sidebarCookie = await JS.InvokeAsync<string>("localStorage.getItem", "sidebar_minimize_state");
+
+        if (sidebarCookie != null)
         {
-            var sidebarCookie = await JS.InvokeAsync<string>("localStorage.getItem", "sidebar_minimize_state");
-
-            if (sidebarCookie != null)
+            SidebarMinimizeState = string.Equals(sidebarCookie, "on");
+            if (SidebarMinimizeState)
             {
-                SidebarMinimizeState = string.Equals(sidebarCookie, "on");
-                if (SidebarMinimizeState)
-                {
-                    KTHelper.addBodyAttribute("data-kt-app-sidebar-minimize", "on");
-                }
+                KTHelper.addBodyAttribute("data-kt-app-sidebar-minimize", "on");
             }
-
-            var themeStyle = await JS.InvokeAsync<string>("localStorage.getItem", "data-bs-theme");
-
-            if (themeStyle != null)
-            {
-                if (themeStyle == "dark")
-                {
-                    KTTheme.SetModeSwitch(true);
-                    KTHelper.addBodyAttribute("data-kt-app-layout", "dark-sidebar");
-                    KTHelper.addBodyClass("sc-theme-dark");
-                    await JS.InvokeVoidAsync("localStorage.setItem", "data-bs-theme", "dark");
-                }
-                else if (themeStyle == "light")
-                {
-                    KTTheme.SetModeSwitch(false);
-                    KTHelper.addBodyAttribute("data-kt-app-layout", "light-sidebar");
-                    KTHelper.addBodyClass("sc-theme-light");
-                    await JS.InvokeVoidAsync("localStorage.setItem", "data-bs-theme", "light");
-                }
-                
-            }
-            IsLoading = false;
-            await InvokeAsync(StateHasChanged);
         }
+
+        var themeStyle = await JS.InvokeAsync<string>("localStorage.getItem", "data-bs-theme");
+
+        if (themeStyle != null)
+        {
+            if (themeStyle == "dark")
+            {
+                KTTheme.SetModeSwitch(true);
+                KTHelper.addBodyAttribute("data-kt-app-layout", "dark-sidebar");
+                KTHelper.addBodyClass("sc-theme-dark");
+                await JS.InvokeVoidAsync("localStorage.setItem", "data-bs-theme", "dark");
+            }
+            else if (themeStyle == "light")
+            {
+                KTTheme.SetModeSwitch(false);
+                KTHelper.addBodyAttribute("data-kt-app-layout", "light-sidebar");
+                KTHelper.addBodyClass("sc-theme-light");
+                await JS.InvokeVoidAsync("localStorage.setItem", "data-bs-theme", "light");
+            }
+
+        }
+
+        await Task.Delay(200);
+        await JS.InvokeVoidAsync("document.body.removeAttribute", "data-kt-app-page-loading");
+        IsLoading = false;
+        await InvokeAsync(StateHasChanged);
     }
 }
